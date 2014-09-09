cd3b2dc18e55885cfc8a90b3fd58c4607bacdda8
Spielberg Tan <Spielberg.t@pocketplaylab.com>
2014-09-09 11:02:53 +0800

    Some example scenes. Text file with differences between UGUI & NGUI.

---
+++ Assets/NGUI/Examples/Scripts/TestScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour {
+
+	public Text tex;
+
+	public void TestFunction(float f)
+	{
+		tex.text=f.ToString();
+	}
+}
