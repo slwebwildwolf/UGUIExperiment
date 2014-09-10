fac7360169e1bbc5c7000548847185082b6c156b
Spielberg Tan <Spielberg.t@pocketplaylab.com>
2014-09-10 11:18:00 +0800

    updated some scripts

--- Assets/Game/Scripts/DragDropScene.cs
+++ Assets/Game/Scripts/DragDropScene.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class DragDropScene : MonoBehaviour {
+
+public class DragDropScene : MonoBehaviour,IDragHandler,IPointerDownHandler,IPointerUpHandler {
 
 	[SerializeField] GameObject grid=null,rootCanvas=null;
 
-	public void Dragging()
+
+
+	public void OnDrag(PointerEventData eventData)
 	{
-		//Debug.Log(transform.position);
 		GetComponent<RectTransform>().pivot.Set(0,0);
 		transform.position=Input.mousePosition;
 	}
 	
-	public void Clicked()
+	public void OnPointerDown(PointerEventData eventData)
 	{
 		transform.localScale=new Vector3(0.7f,0.7f,0.7f);
 		transform.parent=rootCanvas.transform;
 	}
 	
-	public void UnClicked()
+	public void OnPointerUp(PointerEventData eventData)
 	{
 		transform.localScale=new Vector3(1f,1f,1f);
-
+	
 		RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition,-Vector2.up);
 		if (hit.collider != null) {
 			if(hit.collider.gameObject.name=="grid")
@@ -30,6 +33,7 @@ public class DragDropScene : MonoBehaviour {
 		}
 
 	}
+
 
 
 
--- Assets/Game/Scripts/DraggableObjectScene.cs
+++ Assets/Game/Scripts/DraggableObjectScene.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-public class DraggableObjectScene : MonoBehaviour {
+using UnityEngine.EventSystems;
+
+public class DraggableObjectScene : MonoBehaviour,IDragHandler,IPointerDownHandler,IPointerUpHandler{
 
 
 
-	public void Dragging()
+	public void OnDrag(PointerEventData eventData)
 	{
 		GetComponent<RectTransform>().pivot.Set(0,0);
 		transform.position=Input.mousePosition;
 	}
 	
-	public void Clicked()
+	public void OnPointerDown(PointerEventData eventData)
 	{
 		transform.localScale=new Vector3(0.7f,0.7f,0.7f);
 	}
 
-	public void UnClicked()
+	public void OnPointerUp(PointerEventData eventData)
 	{
 		transform.localScale=new Vector3(1f,1f,1f);
 	}
+
+
 }
